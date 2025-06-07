6ea04c43dd556f58b1cfdaa59f0641c2e6db4c45
lilliputten <lilliputten@gmail.com>
2025-06-07 22:56:47 +0300

    Issue #1: Updated project configuration.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+import './index.css';
+
+import App from './App';
+
+const node = document.getElementById('root');
+
+createRoot(node!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>,
+);
