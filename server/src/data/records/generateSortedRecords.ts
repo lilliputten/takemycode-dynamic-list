3acf5211a3dcffe67a0184e8d256afb10d6c70a5
lilliputten <lilliputten@gmail.com>
2025-06-13 00:05:34 +0300

    Issue #18: Optimized data generation on server: using only ids to filter and re-order the data, and only final ids list is converting to full-fledge records data (id & text).
    See `src/data/records/generateSortedRecords.ts`.

--- server/src/data/records/generateSortedRecords.ts
+++ server/src/data/records/generateSortedRecords.ts
@@ -2,14 +2,21 @@ import { TRecord } from '@/shared-types/TRecord';
 import { TRecordsData } from '@/shared-types/TRecordsData';
 import { TSortedRecord } from '@/shared-types/TSortedRecord';
 
-import { generateOneRecord } from './generateOneRecord';
-
 interface TGenerateSortedRecordsParams {
   start: number;
   count: number;
   totalCount: number;
   sortedRecords: TSortedRecord[];
   filter?: string;
+}
+
+function getIdForIndex(index: number) {
+  return index + 1;
+}
+
+export function createRecordById(id: number) {
+  const text = `Item ${id}`;
+  return { id, text } as TRecord;
 }
 
 export function generateSortedRecords(params: TGenerateSortedRecordsParams) {
@@ -24,18 +31,19 @@ export function generateSortedRecords(params: TGenerateSortedRecordsParams) {
 
   // Create filter lambda
   const filterCmp = filter?.toLowerCase();
-  const isRecordPassFilter = (record: TRecord) =>
-    !filterCmp || record.text.toLowerCase().includes(filterCmp);
+  const isProtoPassFilter = !filterCmp
+    ? () => true
+    : (id: number) => String(id).includes(filterCmp);
 
-  // Generate all the available records...
-  const availRecords: TRecord[] = [];
-  for (let realIdx = 0; realIdx < totalCount; realIdx++) {
-    const record = generateOneRecord(realIdx);
+  // Generate only ids first and only later convert them to real data (use more lightweight data)
+  const availIds: number[] = [];
+  for (let index = 0; index < totalCount; index++) {
+    const id = getIdForIndex(index);
     // Check if pass filter?
-    if (!isRecordPassFilter(record)) {
+    if (!isProtoPassFilter(id)) {
       continue;
     }
-    availRecords.push(record);
+    availIds.push(id);
   }
 
   // Sort records...
@@ -44,22 +52,22 @@ export function generateSortedRecords(params: TGenerateSortedRecordsParams) {
       // Do nothing if ids are the same
       continue;
     }
-    const recordIndex = availRecords.findIndex(({ id }) => id === record_id);
-    const targetIndex = availRecords.findIndex(({ id }) => id === target_id);
-    if (recordIndex === -1 || targetIndex === -1) {
-      // Don't throw an error: we're looking in the filtered data, something might be missed, that's ok.
-      continue;
+    // Swap items if both of them are available. Don't throw an error if no
+    // items found: we're looking in the filtered data, something might be
+    // missed, that's ok.
+    const recordIndex = availIds.indexOf(record_id);
+    const targetIndex = availIds.indexOf(target_id);
+    if (recordIndex !== -1 && targetIndex !== -1) {
+      availIds.splice(targetIndex, 0, availIds.splice(recordIndex, 1)[0]);
     }
-    // Swap records
-    availRecords.splice(targetIndex, 0, availRecords.splice(recordIndex, 1)[0]);
   }
 
   // Get amount of available records and the last (stop) index of the required records portion...
-  const availCount = availRecords.length;
+  const availCount = availIds.length;
   const stop = Math.min(availCount, start + count);
 
   // Get slice...
-  const records = availRecords.slice(start, stop);
+  const records = availIds.slice(start, stop).map(createRecordById);
 
   const recordsData: TRecordsData = {
     start,
